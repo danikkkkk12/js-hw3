e5fd27f594c187948e229723164e28357a8d3f48
danikkkkk12 <danilmalyk20@gmail.com>
2024-10-08 10:16:03 +0300

    +

---
+++ index.js
@@ -0,0 +1,38 @@
+let result;
+
+result = 5+5+"5";
+
+console.log(typeof result);
+
+let email = "danilmalyk20@gmail.com";
+
+let containsAtSymbol = email.includes("@");
+
+console.log(`Чи містить email символ '@'? ${containsAtSymbol}`);
+
+let totalCharacters = email.length;
+
+console.log(`Загальна кількість символів в email:  ${totalCharacters}`);
+
+let word1 = "My";
+let word2 = "name";
+let word3 = "is";
+
+let fullName = word1 + " " + word2 + " " + word3;
+fullName += " Viktor";
+
+console.log(fullName);
+
+let userName = "Олександр";
+let payment = 300;
+
+alert(`Дякуємо, ${userName}! До сплати ${payment} гривень`);
+
+
+
+
+
+
+
+
+
